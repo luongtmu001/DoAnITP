98838dc297774f48388cbe2f61b86c3f9213ac78
Hoang Luong <luonghsbg@gmail.com>
2022-04-26 02:36:51 +0700

    Do An

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// show and hide search 
+let cartList = document.querySelector('.cart-list');
+document.querySelector('#cart-btn').onclick = () => {
+    cartList.classList.toggle('active');
+    
+}
